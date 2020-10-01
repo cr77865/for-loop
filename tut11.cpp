e7e132c649c821b3b0b652391a54f9d19fcd4c2d
cr7 <55507597+cr77865@users.noreply.github.com>
2020-10-01 22:30:05 +0530

    Add files via upload

---
+++ tut11.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // for(int i=1;i<8;i++)
+    // {
+    //     if(i==4)
+    //     {
+    //         break;
+    //     }
+    //     cout<<i<<endl;
+    // }
+
+
+
+     for(int i=1;i<8;i++)
+    {
+        if(i==4)
+        {
+            continue;
+        }
+        cout<<i<<endl;
+    }
+    return 0;
+}
